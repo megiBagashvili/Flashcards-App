7d5c2e195c484eabc421fc08364cf985f9bde684
megiBagashvili <megibagashvili2004@gmail.com>
2025-05-01 22:23:20 +0400

    updated jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,10 @@
+/**
+ * @file backend/jest.config.js
+ * @description Configuration file for the Jest testing framework.
+ * Specifies how Jest should discover and run tests for the backend project,
+ * particularly configuring it to work with TypeScript using ts-jest.
+ */
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
